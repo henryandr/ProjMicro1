24170c155c05828119461a762667534716cae112
Henry Andrade <henry.andrade@upb.edu.co>
2024-01-25 17:07:51 -0500

    moficiacion 2

--- main.c
+++ main.c
@@ -4,5 +4,9 @@ int main(void)
 {
     printf("La vida es bella\n");
     printf("El feo es uno\n");
+    for (int i = 0; i < 100; i++)
+    {
+        printf("hola\n");
+    }
     exit(EXIT_SUCCESS);
 }
